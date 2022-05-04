2b92254dd9cfdc05a48c2522cf47665c2dddf2a2
Luo Tian <lt@basecity.com>
2022-05-04 09:20:32 +0800

    decode experiment

--- src/robertluo/pullable/schema.clj
+++ src/robertluo/pullable/schema.clj
@@ -10,15 +10,26 @@
      ::named-var    [:and :symbol [:fn '(fn [v] (re-matches #"\?.+" (name v)))]]
      ::logical-var  [:orn [:wild ::wild-var] [:named ::named-var]]
      ::option       [:cat :keyword :any]
-
      ::qk           [:orn [:k [:not sequential?]] [:list [:cat :any [:* ::option]]]]
      ::qv           [:orn [:var ::logical-var] [:joining [:ref ::pattern]]]
      ::vec          [:map-of ::qk [:ref ::qv]]
      ::seq          [:cat ::vec [:? ::logical-var] [:* ::option]]
-     ::pattern      [:or {:decode/query {:compile (fn [s _] (constantly s))}} [:ref ::vec] [:ref ::seq]]}}
+     ::pattern      [:or [:ref ::vec] [:ref ::seq]]}}
    ::pattern])
 
 (def query-parser (m/parser pattern-schema))
+
+(defn xformer []
+  (mt/transformer
+   {:decoders {:map-of #(update % :a str)}}))
+
+(def decorder (m/decoder pattern-schema xformer))
+
+(comment
+  (update '{:a ?} :a str)
+  (m/decode pattern-schema '{:a ?} xformer)
+  (decorder '{:a ?})
+  )
 
 (def query-decoder (m/decoder pattern-schema
                               (mt/transformer
